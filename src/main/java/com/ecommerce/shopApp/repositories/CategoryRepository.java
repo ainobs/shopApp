1362bcf0894e4e347aae7c28641e5b4bfde3489e
Victor <viktornobe@gmail.com>
2024-11-26 15:30:11 +0000

    added repository and database

---
+++ src/main/java/com/ecommerce/shopApp/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.ecommerce.shopApp.repositories;
+
+import com.ecommerce.shopApp.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
